833b09cfac8b8067eedb32164fa3d2f434a9597e
Alex Su <alex332233@gmail.com>
2023-12-10 22:28:40 +0800

    submit compound HW2

--- section3/CompoundPractice/scripts/CompoundHW.s.sol
+++ section3/CompoundPractice/scripts/CompoundHW.s.sol
@@ -2,46 +2,50 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-// import { TestERC20 } from "../contracts/test/TestERC20.sol";
-// import { SimpleSwap } from "../contracts/SimpleSwap.sol";
 import {CErc20Delegator} from "../contracts/CErc20Delegator.sol";
-// import {} "../contracts/CTokenInterfaces.sol";
-// import "../contracts/ComptrollerInterface.sol";
-// import "../contracts/InterestRateModel.sol";
-// import "../contracts/ErrorReporter.sol";
-// import "../contracts/EIP20NonStandardInterface.sol";
 import {CErc20Delegate} from "../contracts/CErc20Delegate.sol";
 import {Unitroller} from "../contracts/Unitroller.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import {USDT} from "../contracts/USDT.sol";
-import {AlexErc20} from "../contracts/AlexErc20.sol";
+import {AlexErc20, AlexErc20TokenB} from "../contracts/AlexErc20.sol";
 import {Comptroller} from "../contracts/Comptroller.sol";
 import {ComptrollerInterface} from "../contracts/ComptrollerInterface.sol";
 import {SimplePriceOracle} from "../contracts/SimplePriceOracle.sol";
 import {WhitePaperInterestRateModel} from "../contracts/WhitePaperInterestRateModel.sol";
+import {CToken} from "../contracts/CToken.sol";
 
 contract MyScript is Script {
-    function run() external {
-        vm.startBroadcast();
+    ERC20 public tokenA;
+    ERC20 public tokenB;
+    CErc20Delegator cTokenA;
+    CErc20Delegator cTokenB;
+    Unitroller unitroller;
+    SimplePriceOracle priceOracle;
+
+    function run() public {
+        // vm.startBroadcast(); // for real network
         // deployment parameters
-        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         // string memory rpcUrl = vm.envString("RPC_URL");
 
         // Deploying Comptroller
         Comptroller comptroller = new Comptroller();
-        Unitroller unitroller = new Unitroller();
+        unitroller = new Unitroller();
         unitroller._setPendingImplementation(address(comptroller));
         comptroller._become(unitroller);
 
+        // Deploying Comptroller Prxoy
+        Comptroller comptrollerProxy = Comptroller(address(unitroller));
+
         // Deploying PriceOracle
-        SimplePriceOracle priceOracle = new SimplePriceOracle();
-        Comptroller comptrollerOracle = Comptroller(address(unitroller));
-        comptrollerOracle._setPriceOracle(priceOracle);
+        priceOracle = new SimplePriceOracle();
+        comptrollerProxy._setPriceOracle(priceOracle);
 
         // delegator parameters
-        // address underlying_ = address(new USDT());
-        ERC20 erc20Token = new AlexErc20();
-        address underlying_ = address(erc20Token);
+        tokenA = new AlexErc20();
+        tokenB = new AlexErc20TokenB();
+        // ERC20 erc20Token = new AlexErc20();
+        address underlyingA_ = address(tokenA);
+        address underlyingB_ = address(tokenB);
         ComptrollerInterface comptroller_ = ComptrollerInterface(
             address(unitroller)
         );
@@ -51,10 +55,10 @@ contract MyScript is Script {
                 0
             );
         uint initialExchangeRateMantissa_ = 1e18;
-        // string memory name_ = "Compound USDT";
-        // string memory symbol_ = "cUSDT";
-        string memory name_ = "Compound AlexErc20";
-        string memory symbol_ = "cAE2";
+        string memory nameA_ = "Compound AlexErc20";
+        string memory nameB_ = "Compound AlexErc20TokenB";
+        string memory symbolA_ = "cAE2";
+        string memory symbolB_ = "cAE2B";
         uint8 decimals_ = 18;
         address payable admin_ = payable(
             0xC740E68B06383628c6aB9d046dD23E68EcEec19e
@@ -62,25 +66,46 @@ contract MyScript is Script {
         address implementation_ = address(new CErc20Delegate());
         bytes memory becomeImplementationData = "";
 
-        // TestERC20 tokenA = new TestERC20("tokenA", "TKA");
-        // TestERC20 tokenB = new TestERC20("tokenB", "TKB");
-        // SimpleSwap simpleSwap = new SimpleSwap(
-        //     address(tokenA),
-        //     address(tokenB)
-        // );
-        CErc20Delegator cerc20delegator = new CErc20Delegator(
-            underlying_,
+        cTokenA = new CErc20Delegator(
+            underlyingA_,
             comptroller_,
             interestRateModel_,
             initialExchangeRateMantissa_,
-            name_,
-            symbol_,
+            nameA_,
+            symbolA_,
             decimals_,
             admin_,
             implementation_,
             becomeImplementationData
         );
-        vm.stopBroadcast();
-        console.log("Compound address: %s", address(cerc20delegator));
+        cTokenB = new CErc20Delegator(
+            underlyingB_,
+            comptroller_,
+            interestRateModel_,
+            initialExchangeRateMantissa_,
+            nameB_,
+            symbolB_,
+            decimals_,
+            admin_,
+            implementation_,
+            becomeImplementationData
+        );
+
+        // add Market to Markets
+        comptrollerProxy._supportMarket(CToken(address(cTokenA)));
+        comptrollerProxy._supportMarket(CToken(address(cTokenB)));
+
+        // set tokenA and tokenB price
+        priceOracle.setUnderlyingPrice(CToken(address(cTokenA)), 1e18); // 1USD
+        priceOracle.setUnderlyingPrice(CToken(address(cTokenB)), 1e20); // 100USD
+
+        // set collateralFactor, closeFactor, liquidationIncentive
+        comptrollerProxy._setCollateralFactor(CToken(address(cTokenB)), 5e17); // 50%
+        comptrollerProxy._setCloseFactor(5e17); // 50%
+        comptrollerProxy._setLiquidationIncentive(1.08 * 1e18); // 8%
+
+        // vm.stopBroadcast(); // for real network
+        console.log("cTokenA address: %s", address(cTokenA));
+        console.log("cTokenB address: %s", address(cTokenB));
     }
 }
